3ec8175bc5f9f54cf41e7eca2d504ac13211a7f3
lnguptavema <nareshgupta2294@gmail.com>
2025-08-08 17:05:15 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,3 +1,47 @@
+//my code
+import {Component} from 'react'
+
+import './index.css'
+
+class EvenOddApp extends Component {
+  state = {
+    counter: 0,
+    ceilNo: Math.ceil(Math.random(0, 100) * 100)
+  }
+
+  onIncrement = () => {
+    this.setState(prevState => {
+      return {counter: prevState.counter + Math.ceil(Math.random(0, 100) * 100)}
+    })
+  }
+
+  render() {
+    const {counter} = this.state
+    let valued
+    if (counter % 2 == 0) {
+      valued = <p>Count is Even</p>
+    } else {
+      valued = <p>Count is Odd</p>
+    }
+
+    return (
+      <div className="maincontainer">
+        <div className="container1">
+          <h1>Count {counter}</h1>
+          {valued}
+          <button onClick={this.onIncrement} type="button">
+            Increment
+          </button>
+          <p>*Increase By Random Number Between 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
+
+//
 import {Component} from 'react'
 
 import './index.css'
